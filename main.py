475ced1de5c326c604e12cfe1247b87d7ce9269f
Heilong-Lark <54684355+Heilong-Lark@users.noreply.github.com>
2024-08-13 16:15:10 -0700

    setting up again

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ from disnake.ext import commands
 load_dotenv()  # Take environment variables from .env.
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+TEST = os.getenv('TEST_GUILDS')
 
-bot = commands.Bot(test_guilds=[922934220056330271])
-
+bot = commands.Bot(test_guilds=[TEST])
 
 @bot.event
 async def on_ready():
     print("The bot is ready!")
-
 
 bot.load_extensions("cogs")
 bot.run(TOKEN)
